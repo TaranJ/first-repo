453588e8bdfbdeffa93ff5b508ad6993fa8b22d1
TaranJ <taran.91@hotmail.com>
2023-09-01 15:18:26 +0200

    replaced html with js on index.page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,44 @@
 const url = "https://api.noroff.dev/api/v1/gamehub";
 
-async function makeApiCall() {
+const newReleases = document.querySelector(".new-releases");
+const specialOffers = document.querySelector(".game-list");
+
+// fetches game images for new releases on home page
+async function getGameImages() {
   const response = await fetch(url);
+  const games = await response.json();
 
-  const results = await response.json();
+  console.log(games);
 
-  console.log(results);
+  for (let i = 0; i < games.length; i++) {
+    if (i === 5) {
+      break;
+    }
+
+    newReleases.innerHTML += `<img src="${games[i].image}">`;
+  }
 }
 
-makeApiCall();
+getGameImages();
+
+// only shows games on sale
+async function getGamesOnSale() {
+  const response = await fetch(url);
+  const games = await response.json();
+
+  for (let i = 0; i < games.length; i++) {
+    if (games[i].onSale === false) {
+      continue;
+    }
+    if (i === 8) {
+      break;
+    }
+
+    specialOffers.innerHTML += ` <div class="pricing"><a href="product.html"><img src="${games[i].image}"></a>
+                                <p class="sale">Sale</p>
+                                <p class="old-price">€${games[i].price}</p>
+                                <p class="sale-price">€${games[i].discountedPrice}</p></div>`;
+  }
+}
+
+getGamesOnSale();
